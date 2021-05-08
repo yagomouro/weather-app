0e4ded25af081c92bc7be2069e8b69fe34b2e2dd
pednucci <pedropetenucci@gmail.com>
2021-05-08 17:55:35 -0300

    node implementation

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+require('dotenv').config();
+const { format } = require('date-fns');
+const { ptBR, enUS } = require('date-fns/locale');
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'html');
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.get('/key', (req, res) => {
+    res.json({
+        key: process.env.KEY
+    })
+})
+
+app.get('/date', (req, res) => {
+    let dateNow = Date.now();
+    let formatDate = format(dateNow, "EEEE, dd 'de' MMMM", {locale: ptBR});
+    res.json({
+        date: formatDate
+    })
+})
+
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
---
+++ public/js/main.js
@@ -0,0 +1,58 @@
+var toggleBtn = document.querySelector('#switch');
+var docElement = document.documentElement;
+var theme = docElement.getAttribute('data-theme');
+var cep = document.getElementById("inputCEP");
+
+toggleBtn.addEventListener('change', () => {
+  if (toggleBtn.checked) {
+    transition()
+    docElement.setAttribute('data-theme', 'dark')
+  } else {
+    transition()
+    docElement.setAttribute('data-theme', 'light')
+  }
+})
+
+let transition = () => {
+  docElement.classList.add('transition');
+  window.setTimeout(() => {
+    docElement.classList.remove('transition')
+  }, 1000)
+}
+
+fetch('http://localhost:3000/date', {
+  method: 'GET'
+}).then(response => response.json().then(result => {
+  const date = result.date;
+  const totalD = date.split(' ');
+  for(i = 0; i < totalD.length; i++){
+    let w = totalD[i];
+    if(w != 'de') totalD[i] = w[0].toUpperCase() + w.slice(1);
+  }
+  document.getElementById("data").innerText = totalD.join(" ");
+}))
+
+cep.addEventListener('keyup', event => {
+  if(cep.value.length == 8) {
+    fetch(`https://viacep.com.br/ws/${cep.value}/json/`, {
+      method: 'GET'
+    }).then(response => response.json().then(result => {
+      if(result.erro){
+        document.getElementById('local').innerHTML = `<strong>Cidade não encontrada!</strong>`
+        return;
+      }
+      document.getElementById('local').innerHTML = `<strong>${result.localidade}, ${result.uf} -</strong> Brasil`;
+      const city = result.localidade;
+      fetch('http://localhost:3000/key', {
+        method: 'GET'
+      }).then(result => result.json().then(response => {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=pt&units=metric&appid=${response.key}`)
+        .then(result => result.json().then(response => {
+          document.getElementById('temp').innerText = `${response.main.temp}º`;
+          document.getElementById('wing').innerText = `${response.wind.speed} m/s`;
+          document.getElementById('humidity').innerText = `${response.main.humidity}%`
+        }))
+      }))
+    }))
+  }
+})
